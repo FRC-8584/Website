714b307bdfe40b755d1689512d11fea12c65125c
AloneAlongLife <xd0931021@gmail.com>
2022-10-01 11:11:33 +0800

    Auto Commit

--- dashboard/static/js/index.js
+++ dashboard/static/js/index.js
@@ -2,15 +2,59 @@ function sleep(ms) {
     return new Promise(resolve => setTimeout(resolve, ms));
 }
 
-function onload() {
-    return;
+function index_onload() {
+    let element_list = document.querySelectorAll(".content");
+    if (element_list.length == 0) {
+        setTimeout(index_onload, 100);
+        return;
+    }
+    for (let i = 0; i < element_list.length; i++) {
+        let element = element_list[i];
+        if (element.innerHTML == "") {
+            setTimeout(index_onload, 100);
+            return;
+        }
+        element.style.setProperty("--sticky_top", `${window.innerHeight - element.getBoundingClientRect().height}px`);
+    }
 }
 
 function scroll_change() {
-    if (window.scrollY < document.querySelector("body > img").height + 64) {
+    if (document.querySelectorAll(".button-div").length == 0) {
+        setTimeout(scroll_change, 100);
+        return;
+    }
+
+    let scroll_position = window.scrollY;
+    if (scroll_position <= document.querySelector("body > img").height + 64) {
         document.querySelector("#top-bar").style.setProperty("--trans", 1);
     }
     else {
         document.querySelector("#top-bar").style.setProperty("--trans", 0);
+    }
+    let img_blur = 10 * window.scrollY / (document.querySelector("body > img").height + 64);
+    document.querySelector("img.title-img").style.setProperty("--blur", `${img_blur}px`)
+
+    let page_list = document.querySelectorAll(".content");
+    let start_position = window.innerHeight * 0.9;
+    for (let i = page_list.length - 1; i >= 0; i--) {
+        let element = page_list[i];
+        let element_position = element.getBoundingClientRect().top;
+        if (element_position < start_position) {
+            element.style.setProperty("--blur", "0px");
+            let button_list = document.querySelectorAll(".button-div");
+            for (let j = 0; j < button_list.length; j++) {
+                if (j == i) {
+                    button_list[j].classList.add("select")
+                }
+                else {
+                    button_list[j].classList.remove("select")
+                }
+            }
+            if (i > 0) {
+                let img_num = 15 * (start_position - element_position) / start_position;
+                element.previousElementSibling.style.setProperty("--blur", `${img_num}px`);
+            }
+            break;
+        }
     }
 }
